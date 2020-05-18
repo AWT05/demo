247f98810090efe4882619b424d5aacab9418b4f
Carlos Gonzales <carlos.gonzales@jalasoft.com>
2020-05-18 10:04:33 -0400

    Initial code

---
+++ src/test/java/org/fundacion/demo/Demo.java
@@ -0,0 +1,29 @@
+package org.fundacion.demo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void testLoginTrello() {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+
+		driver.manage().window().setSize(new Dimension(414, 736));
+
+		driver.get("https://trello.com/login");
+
+		driver.findElement(By.cssSelector("#user")).sendKeys("carledriss3");
+		driver.findElement(By.cssSelector("#password")).sendKeys("P@ssw0rd");
+		driver.findElement(By.cssSelector("input.account-button")).click();
+
+		driver.close();
+
+		driver.quit();
+	}
+
+}
